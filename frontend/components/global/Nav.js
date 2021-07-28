4a294f0654d381505a2d04fb3867f5f90cdd5168
Ashar Farooq <ashar744@gmail.com>
2021-07-28 17:54:44 -0400

    Updated the Navigation Bar
    Co-Authored-By: Joshua Feliciano <41080599+joshfeli@users.noreply.github.com>
    Co-Authored-By: irinazoccolini <71565864+irinazoccolini@users.noreply.github.com>

--- frontend/components/global/Nav.js
+++ frontend/components/global/Nav.js
@@ -1,22 +1,21 @@
-
 import React from "react";
 import STYLES from "./Nav.module.scss";
-import DH_LOGO from "../../images/dh_logo.svg";
 
 const Nav = () => {
 
     return (
-        <nav className={STYLES.nav}>
-            <a className={STYLES.linkHome} href="/">
-                Home
-            </a>
-            <a className={STYLES.linkHome} href="/sentimentanalysis">
-                Sentiment Analysis
-            </a>
-            <a className={STYLES.linkLab} href="https://digitalhumanities.mit.edu/" target="_blank" rel="noreferrer">
-                <img className={STYLES.imageLab} src={DH_LOGO} />
-            </a>
+        <>
+        <nav className="navbar navbar-light bg-light navbar-expand-sm">
+            <a className={`navbar-brand ${STYLES.linkHome}`} href="/">Sonification</a>
+            <ul className="navbar-nav mr-auto">
+                <li className="nav-item">
+                    <a className="nav-link" href="/sentimentanalysis">
+                        Sentiment Analysis
+                    </a>
+                </li>
+            </ul>
         </nav>
+        </>
     );
 };
 

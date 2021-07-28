c93bcd64303b6bc7ff7c284878bf8f4c7ebfceae
Yifan Wang <yifan.wang0422@gmail.com>
2021-07-28 17:14:45 -0400

    deleted loading state bc it'll be used later for audio

--- frontend/components/SentimentAnalysis.js
+++ frontend/components/SentimentAnalysis.js
@@ -3,19 +3,10 @@ import * as PropTypes from "prop-types";
 import STYLES from "./SentimentAnalysis.module.scss";
 
 const SentimentAnalysis = () => {
-    const [sentimentAnalysisData, setSentimentAnalysisData] = useState([]);
-    const [loading, setLoading] = useState(true);
     const [userInput, setUserInput] = useState("");
     const [submitted, setSubmitted] = useState(false);
-
-    useEffect(() => {
-        fetch("/api/sentiment_analysis")
-            .then(response => response.json())
-            .then(data => {
-                setLoading(false);
-                setSentimentAnalysisData(data);
-            });
-    }, []);
+    const [loading, setLoading] = useState(true);
+    const [audioData, setAudioData] = useState(null);
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -23,20 +14,15 @@ const SentimentAnalysis = () => {
 
     return(
         <div className="container-fluid">
-            {loading
-                ? <p>Currently loading Sentiment Analysis page...</p>
-                : <div>
-                    <form onSubmit={handleSubmit}>
-                        <div className="form-group">
-                            <label>
-                                Please input your response in the textarea below.
-                            </label>
-                            <textarea className="form-control" id="text" rows="5" required></textarea>
-                        </div>
-                        <button type="submit" className="btn btn-primary">Submit</button>
-                    </form>
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label>
+                        Please input your response in the textarea below.
+                    </label>
+                    <textarea className="form-control" id="text" rows="5" required></textarea>
                 </div>
-            }
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
         </div>
     );
 };

40178a830731750b0e6af4bc7f88ee220eeabd44
Joshua Feliciano <joshfeli613@gmail.com>
2021-08-02 17:09:38 -0400

    First draft of `change_volume`
    Concerns: `audio` is an int16 array, so this may lead to rounding and quantization errors.
    Co-Authored-By: irinazoccolini <71565864+irinazoccolini@users.noreply.github.com>
    Co-Authored-By: Ashar Farooq <ashar744@gmail.com>
    Co-Authored-By: Watermelanie <46464201+Watermelanie@users.noreply.github.com>

--- backend/app/analysis/filters.py
+++ backend/app/analysis/filters.py
@@ -6,15 +6,16 @@ import numpy as np
 from backend.app.analysis import *
 
 
-def change_volume(audio, amplitude, sample_rate=44100):
+def change_volume(audio, amplitude):
     """
     :param audio: An int16 NumPy array representing a list of samples
     :param amplitude: An int representing the factor increase or decrease in volume
-    :param sample_rate: An int representing a sample rate in Hz
+    :return: A new int16 NumPy array with a modified amplitude.
     """
 
-    #TBD
-    return audio, sample_rate
+    # new_audio = amplitude * audio
+
+    return amplitude * audio
 
 
 def change_speed(audio, sample_rate=44100):

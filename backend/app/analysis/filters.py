0673d8c8d10478912cd8cd5082a528fddfecbf87
Joshua Feliciano <joshfeli613@gmail.com>
2021-08-02 16:57:19 -0400

    Implemented skeleton for some filtering functions
    Co-Authored-By: irinazoccolini <71565864+irinazoccolini@users.noreply.github.com>
    Co-Authored-By: Watermelanie <46464201+Watermelanie@users.noreply.github.com>
    Co-Authored-By: Ashar Farooq <ashar744@gmail.com>

---
+++ backend/app/analysis/filters.py
@@ -0,0 +1,53 @@
+""""
+Various filtering functions to apply to audio waves (represented as int16 numpy arrays)
+"""
+
+import numpy as np
+from backend.app.analysis import *
+
+
+def change_volume(audio, amplitude, sample_rate=44100):
+    """
+    :param audio: An int16 NumPy array representing a list of samples
+    :param amplitude: An int representing the factor increase or decrease in volume
+    :param sample_rate: An int representing a sample rate in Hz
+    """
+
+    #TBD
+    return audio, sample_rate
+
+
+def change_speed(audio, sample_rate=44100):
+    """
+    :param audio: An int16 NumPy array representing a list of samples
+    :param sample_rate: An int representing a sample rate in Hz
+    """
+
+    return audio, sample_rate
+
+
+def add_chords(audio, sample_rate=44100):
+    """
+    :param audio: An int16 NumPy array representing a list of samples
+    :param sample_rate: An int representing a sample rate in Hz
+    """
+
+    return audio, sample_rate
+
+
+def overlap_notes(audio, sample_rate=44100):
+    """
+    :param audio: An int16 NumPy array representing a list of samples
+    :param sample_rate: An int representing a sample rate in Hz
+    """
+
+    return audio, sample_rate
+
+
+def change_quality(audio, sample_rate=44100):
+    """
+    :param audio: An int16 NumPy array representing a list of samples
+    :param sample_rate: An int representing a sample rate in Hz
+    """
+
+    return audio, sample_rate

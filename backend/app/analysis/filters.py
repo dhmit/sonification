87e6fd286ac7a08a38a431472fced426f99b44ca
Ashar Farooq <ashar744@gmail.com>
2021-08-03 15:33:15 -0400

    started to change parameters to reflect dict change

--- backend/app/analysis/filters.py
+++ backend/app/analysis/filters.py
@@ -3,52 +3,52 @@ Various filtering functions to apply to audio waves (represented as int16 numpy 
 """
 
 import numpy as np
-from backend.app.analysis import *
 
 
-def change_volume(audio, amplitude):
+def change_volume(audio_metadata, amplitude):
     """
     NOTE: There is more to "loudness" to "amplitude" conversion than merely an amplitude factor.
     We still need to work out this relation to create a filter that's useful to users and developers.
 
-    :param audio: An int16 NumPy array representing a list of samples
+    :param audio_metadata: A Dict instance containing audio samples list, sample rate, and notes array
     :param amplitude: An int representing the factor increase or decrease in volume
-    :return: A new int16 NumPy array with a modified amplitude.
+    :return: A Dict instance containing audio samples list with modified amplitudes, sample rate, and notes array
     """
+
 
     return (amplitude * audio).astype(np.int16)
 
 
-def change_speed(audio, sample_rate=44100):
+def change_speed(audio_metadata, sample_rate=44100):
     """
-    :param audio: An int16 NumPy array representing a list of samples
+    :param audio_metadata: An int16 NumPy array representing a list of samples
     :param sample_rate: An int representing a sample rate in Hz
     """
 
     return audio, sample_rate
 
 
-def add_chords(audio, sample_rate=44100):
+def add_chords(audio_metadata, sample_rate=44100):
     """
-    :param audio: An int16 NumPy array representing a list of samples
+    :param audio_metadata: An int16 NumPy array representing a list of samples
     :param sample_rate: An int representing a sample rate in Hz
     """
 
     return audio, sample_rate
 
 
-def overlap_notes(audio, sample_rate=44100):
+def overlap_notes(audio_metadata, sample_rate=44100):
     """
-    :param audio: An int16 NumPy array representing a list of samples
+    :param audio_metadata: An int16 NumPy array representing a list of samples
     :param sample_rate: An int representing a sample rate in Hz
     """
 
     return audio, sample_rate
 
 
-def change_quality(audio, sample_rate=44100):
+def change_quality(audio_metadata, sample_rate=44100):
     """
-    :param audio: An int16 NumPy array representing a list of samples
+    :param audio_metadata: An int16 NumPy array representing a list of samples
     :param sample_rate: An int representing a sample rate in Hz
     """
 

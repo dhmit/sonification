65f7475893362bc9bf9cc77c30132f75c5578876
Joshua Feliciano <joshfeli613@gmail.com>
2021-08-03 15:00:59 -0400

    Clean up `text_to_sound` function
    Modified code to include fewer loops and return more metadata about the audio

--- backend/app/analysis/sentiment_analysis.py
+++ backend/app/analysis/sentiment_analysis.py
@@ -6,30 +6,39 @@ import numpy as np
 def text_to_sound(user_text):
     """
     Takes text, runs NLTK's sentiment analyzer, and then returns a sound object
+
+    :param user_text: A str representing some input text
+    :return: A dictionary of audio metadata with the following keys:
+        audio_samples: An int16 Numpy array
+        sample_rate: An int, recorded in Hz
+        notes: A list of 2-element tuples (freq, duration) for each note's frequency (Hz) and duration (s) in the audio
+
     """
-
-    nltk_analyzer = SentimentIntensityAnalyzer()
-    all_sentences = nltk.sent_tokenize(user_text)
-
-    all_scores = []
-    for each_sentence in all_sentences:
-        all_scores.append(nltk_analyzer.polarity_scores(each_sentence)["compound"] * 15)
-
-    all_frequencies = []
-    for each_score in all_scores:
-        frequency = 440 * (2 ** (each_score / 12))
-        all_frequencies.append(frequency)
-
+    A4 = 440
     sample_rate = 44100
     T = 1
     t = np.linspace(0, T, T * sample_rate, False)
 
-    all_notes = []
-    for each_freq in all_frequencies:
-        all_notes.append(np.sin(each_freq * t * 2 * np.pi))
+    nltk_analyzer = SentimentIntensityAnalyzer()
+    all_sentences = nltk.sent_tokenize(user_text)
 
-    audio = np.hstack(all_notes)
+    sin_waves = []
+    all_notes = []
+
+    for each_sentence in all_sentences:
+        score = nltk_analyzer.polarity_scores(each_sentence)['compound'] * 15
+        frequency = A4 * (2 ** (score / 12))
+        sin_waves.append(np.sin(2 * np.pi * frequency * t))
+        all_notes.append((frequency, T))
+
+    audio = np.hstack(sin_waves)
     audio *= 32767 / np.max(np.abs(audio))
     audio = audio.astype(np.int16)
 
-    return audio, sample_rate
+    audio_metadata = {
+        'audio_samples': audio,
+        'sample_rate': sample_rate,
+        'notes': all_notes
+    }
+
+    return audio_metadata

d982d09678f806cf4dcc7f90ed79ef7cfbab7648
jaslarus <85576329+j-aslarus@users.noreply.github.com>
2021-08-04 15:58:01 -0400

    Created a function to generate more realistic sounding notes

--- backend/app/analysis/image_to_sound.py
+++ backend/app/analysis/image_to_sound.py
@@ -1,6 +1,44 @@
 import numpy as np
 import simpleaudio as sa
 
+
+def generate_note(frequency, duration, sample_rate):
+    """
+    Uses the ADSR (Attack, Decay, Sustain, Release) envelope to
+    generate a note that fades in and out realistically
+    Adapted from example in https://towardsdatascience.com/music-in-python-2f054deb41f4
+    :param frequency: frequency of the note
+    :param duration: duration in seconds
+    :param sample_rate: sampling rate
+    :return note: list of samples for the note
+    """
+    a_percentage = 0.1
+    d_percentage = 0.1
+    s_percentage = 0.1
+    r_percentage = 0.7
+    peak_weight = 1
+    sustain_weight = 0.7
+    final_weight = 0
+
+    total_length = duration * sample_rate
+    len_a = int(a_percentage * total_length)
+    len_d = int(d_percentage * total_length)
+    len_s = int(s_percentage * total_length)
+    len_r = int(r_percentage * total_length)
+
+    a_weights = [peak_weight * (2**(x/len_a) - 1) for x in range(len_a)]
+    d_weights = [sustain_weight + (peak_weight-sustain_weight) * (2**(1-x/len_d) - 1) for x in range(len_d)]
+    s_weights = [sustain_weight for _ in range(len_s)]
+    r_weights = [final_weight + sustain_weight * (2**(1-x/len_r) - 1) for x in range(len_r)]
+
+    all_weights = np.array(a_weights + d_weights + s_weights + r_weights)
+    time_steps = np.linspace(0, duration, (len_a + len_d + len_s + len_r), False)
+    note = np.sin(frequency * time_steps * 2 * np.pi) * all_weights
+    note *= 32767 / np.max(np.abs(note))
+
+    return note
+
+
 def analyze_image(im):
     """
     :param im: .jpg image to be analyzed

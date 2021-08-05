685da8085ebf650d77387f1fcef1bdc6958d9e96
jaslarus <85576329+j-aslarus@users.noreply.github.com>
2021-08-05 16:39:15 -0400

    Adding to unittests

--- backend/app/tests.py
+++ backend/app/tests.py
@@ -21,6 +21,20 @@ class TextToMusicTestCase(TestCase):
         score2 = {'neg': 0.0, 'neu': 0.536, 'pos': 0.464, 'compound': 0.4939}
         sentence3 = "This ice cream tastes bad and I am disappointed."
         score3 = {'neg': 0.524, 'neu': 0.476, 'pos': 0.0, 'compound': -0.765}
+        sentence4 = "This ice ;cream taSt,,es bad AN.d I am disappointed."
         self.assertEqual(text_to_music._analyse_sentiment(sentence1), score1)
         self.assertEqual(text_to_music._analyse_sentiment(sentence2), score2)
         self.assertEqual(text_to_music._analyse_sentiment(sentence3), score3)
+        self.assertEqual(text_to_music._analyse_sentiment(sentence4), score3)
+
+    def test_generate_note_frequency(self):
+        """
+        Tests the function _generate_note_frequency from text_to_music.py
+        """
+        pass
+
+    def test_sonify_sentence(self):
+        """
+        Tests the function _sonify_sentence from text_to_music.py
+        """
+        pass

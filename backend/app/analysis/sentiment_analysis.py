af08e3d616ea11ed443a58034b1c157186afafac
Joshua Feliciano <joshfeli613@gmail.com>
2021-08-10 14:00:33 -0400

    Updated docstring/comments in `text_to_sound` function

--- backend/app/analysis/sentiment_analysis.py
+++ backend/app/analysis/sentiment_analysis.py
@@ -5,15 +5,12 @@ import numpy as np
 
 def text_to_sound(user_text):
     """
-    Takes text, runs NLTK's sentiment analyzer, and then returns a sound object
+    Takes text, runs NLTK's sentiment analyzer, and then returns a sound object.
+    The formula used to generate a note frequency is based off of this article:
+    <https://towardsdatascience.com/music-in-python-2f054deb41f4>.
 
     :param user_text: A str representing some input text
-    :return: A dictionary of audio metadata with the following keys:
-        audio_samples: An int16 Numpy array
-        sample_rate: An int, recorded in Hz
-        notes: A list of 3-element tuples (freq, duration, score) for each note's frequency (Hz) and duration (s)
-            in the audio, as well as the score used to acquire that note
-
+    :return: A tuple of audio data containing an int16 NumPy array of samples and a sample rate in Hz (int).
     """
     A4 = 440
     sample_rate = 44100
@@ -29,8 +26,11 @@ def text_to_sound(user_text):
         # the compound score ranges from -1 to 1, this shifts the score so that there is no negative score
         # multiplying by 44 and rounding the score gives us a range of keys from 0 to 88
         score = round((nltk_analyzer.polarity_scores(each_sentence)['compound'] + 1) * 44)
+
         # formula calculates the frequency of a musical note using A4 as the base note
-        # the score is the nth key starting from key 0 being A0
+        # the score is the nth key starting from key 0 being A0 (hence the extra +1)
+        # the exact middle of the piano is between key 43 (E4) and 44 (F4) if A0 is the zeroth note
+        # thus, a neutral score should generate an F4 note
         frequency = A4 * (2 ** ((score + 1 - 49) / 12))
         sin_waves.append(np.sin(2 * np.pi * frequency * t))
 

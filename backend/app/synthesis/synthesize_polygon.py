b58281950c4d17189d25f1375d7682636c8da8ca
Quincy Johnson <35878908+qujohn314@users.noreply.github.com>
2021-10-01 15:51:19 -0400

    Update synthesize_polygon.py
    WIP angle calculation implementation

--- backend/app/synthesis/synthesize_polygon.py
+++ backend/app/synthesis/synthesize_polygon.py
@@ -1,4 +1,7 @@
 # hard-coded data
+import math
+import numpy as np
+
 square = [(0, 0), (0, 1), (1, 1), (1, 0)]
 
 
@@ -6,9 +9,22 @@ def angles_of_polygon(points):
     """
     Computes the internal angles of this polygon, in input order.
     :param points: A list of points representing a polygon.
-    :return: A list of angles of this polygon.
+    :return: A list of angles of this polygon in degrees.
     """
-    pass
+    vectors = []
+    angles = []
+    for i in range(points-1):
+        vectors.append(np.array[points[i+1][0]-points[i][0],points[i+1][1]-points[i][1]])
+
+    for i in range(vectors-1):
+        mag_v1 = (np.sqrt(vectors[i].dot(vectors[i])))
+        mag_v2 = (np.sqrt(vectors[i+1].dot(vectors[i+1])))
+        angles.append(math.acos(vectors[i].dot(vectors[i+1])/(mag_v1*mag_v2)))
+
+    return angles
+
+
+
 
 
 def sides_of_polygons(points):

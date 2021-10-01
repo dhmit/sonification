ac9d6e635da247d0524e7885586c7598af65efde
vedadehhc <51378145+vedadehhc@users.noreply.github.com>
2021-10-01 15:18:27 -0400

    Create synthesize_polygon.py

---
+++ backend/app/synthesis/synthesize_polygon.py
@@ -0,0 +1,12 @@
+# hard-coded data
+square = [(0, 0), (0, 1), (1, 1), (1, 0)]
+
+
+def synthesize_polygon(points):
+    """
+    Synthesizes a polygon. The polygon is represented as a list of points
+    where each point is a tuple of length 2.
+    :param points: A list of points representing a polygon.
+    :return: A numpy array which represents the sound.
+    """
+    pass

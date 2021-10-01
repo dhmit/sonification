4338d0b9d7bed154d0d11a769f461657d8ce0787
vedadehhc <51378145+vedadehhc@users.noreply.github.com>
2021-10-01 15:25:15 -0400

    Added function headers for computing angles and sides of polygons

--- backend/app/synthesis/synthesize_polygon.py
+++ backend/app/synthesis/synthesize_polygon.py
@@ -2,6 +2,24 @@
 square = [(0, 0), (0, 1), (1, 1), (1, 0)]
 
 
+def angles_of_polygon(points):
+    """
+    Computes the internal angles of this polygon, in input order.
+    :param points: A list of points representing a polygon.
+    :return: A list of angles of this polygon.
+    """
+    pass
+
+
+def sides_of_polygons(points):
+    """
+    Computes the side lengths of this polygon, in input order.
+    :param points: A list of points representing a polygon.
+    :return: A list of side lengths of this polygon.
+    """
+    pass
+
+
 def synthesize_polygon(points):
     """
     Synthesizes a polygon. The polygon is represented as a list of points

a92e7d836b2e2d7cdb0a988b3666ac431f276f8b
Quincy Johnson <35878908+qujohn314@users.noreply.github.com>
2021-10-01 16:03:31 -0400

    Update synthesize_polygon.py
    Finished angle calculation implementations

--- backend/app/synthesis/synthesize_polygon.py
+++ backend/app/synthesis/synthesize_polygon.py
@@ -11,19 +11,28 @@ def angles_of_polygon(points):
     :param points: A list of points representing a polygon.
     :return: A list of angles of this polygon in degrees.
     """
+    points.append(points[0])  # Polygon need to be closed shape.
     vectors = []
     angles = []
-    for i in range(points-1):
-        vectors.append(np.array[points[i+1][0]-points[i][0],points[i+1][1]-points[i][1]])
 
-    for i in range(vectors-1):
+    for i in range(len(points) - 1):
+        arr = [points[i + 1][0] - points[i][0], points[i + 1][1] - points[i][1]]
+        vectors.append(np.array(arr))
+
+    vectors.append(vectors[0])  # Polygon need to be closed shape.
+
+    for i in range(len(vectors) - 1):
         mag_v1 = (np.sqrt(vectors[i].dot(vectors[i])))
-        mag_v2 = (np.sqrt(vectors[i+1].dot(vectors[i+1])))
-        angles.append(math.acos(vectors[i].dot(vectors[i+1])/(mag_v1*mag_v2)))
+        mag_v2 = (np.sqrt(vectors[i + 1].dot(vectors[i + 1])))
+        angles.append(math.acos(vectors[i].dot(vectors[i + 1]) / (mag_v1 * mag_v2)))
+
+    rad_to_deg = map(lambda x: x * 180 / math.pi, angles)
+    angles = list(rad_to_deg)
 
     return angles
 
 
+print(angles_of_polygon(square))
 
 
 def change_in_frequency(angles):
@@ -32,7 +41,8 @@ def change_in_frequency(angles):
         :param angles: A list of angles of a polygon.
         :return: A list of frequencies.
         """
-    return [180/theta for theta in angles]
+    return [180 / theta for theta in angles]
+
 
 def sides_of_polygons(points):
     """

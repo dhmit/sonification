16a5fe496cac3b944dc126ee5dc9e6f7f4b5e881
grace-j784 <gracejau88@gmail.com>
2021-10-01 15:45:35 -0400

    Update gesture.py
    implemented compress_coordinates

--- backend/app/data_processing/gesture.py
+++ backend/app/data_processing/gesture.py
@@ -1,4 +1,19 @@
 
+def compress_coordinates(gesture, factor):
+    """
+    :param gesture: list of coordinates received from user input via front end
+    :param factor: factor by which we compress coordinates
+    :return: list of tuples (x, y) each representing a compressed coordinate, compressed by
+    taking averages of groups of coordinates
+    """
+    result = list()
+    for i in range(0, len(gesture)-factor, factor):
+        x_average = (gesture[i][0] + gesture[i+factor-1][0])/2
+        y_average = (gesture[i][1] + gesture[i+factor-1][1])/2
+        result.append((x_average, y_average))
+    return result
+
+
 def get_sound(gesture):
     """
     Given a list of coordinates representing a single gesture received from user input via

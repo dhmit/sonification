b25f22433f8a58a1b30269b7348d0a665ac67a35
Binh314 <binhp@mit.edu>
2021-10-02 01:12:54 -0400

    Created a function to get average contiguous space length per line
    Also fixed a logic error in the refactored extract_whitespace_info_from_text function.
    
    Also refactored it a bit further by eliminating the if-statement after the end of the for loop in favor of concatenating a non-whitespace character (arbitrarily chose a period) onto the given text.

--- backend/app/data_processing/text.py
+++ backend/app/data_processing/text.py
@@ -49,6 +49,7 @@ def clean_text(text):
     cleaned_text = lower_case.translate(str.maketrans('', '', string.punctuation))
     return cleaned_text
 
+
 def extract_whitespace_info_from_text(text):
     """
     Extract information about whitespace in given text.
@@ -64,19 +65,61 @@ def extract_whitespace_info_from_text(text):
     previous_char = None
     counter = 0
 
-    for char in cleaned_text:
+    for char in cleaned_text+'.': # Add period to cleaned_text to account for trailing whitespaces
         # If contiguous block of whitespace has ended, add info to output and reset counter
-        if char != previous_char and previous_char in whitespace_characters:
-            whitespace_info.append((counter, previous_char))
+        if char != previous_char:
+            if previous_char in whitespace_characters:
+                whitespace_info.append((counter, previous_char))
             counter = 0
 
         previous_char = char
         counter += 1
 
-    # Account for trailing whitespaces
-    if previous_char in whitespace_characters:
-        whitespace_info.append((counter, previous_char))
-
     return whitespace_info
 
 
+def get_average_contiguous_spaces_per_line(text):
+    """
+    Get an array containing average lengths of contiguous spaces per line in given text.
+    :param text: String of text
+    :return: Array of floats, where the ith entry represents the average length of contiguous
+    spaces in the ith line.
+    """
+
+    contig_space_length_averages = []
+
+    # Get whitespace data for easier calculation
+    # Added newline tuple to account for final contiguous whitespace
+    whitespace_info = extract_whitespace_info_from_text(text) + [(0, '\n')]
+
+    # Keep track of the numbers of total spaces
+    current_total_spaces = 0
+    # Keep track of the number of contiguous space blocks
+    current_num_contig_spaces = 0
+
+    for whitespace in whitespace_info:
+
+        # Get info about the contiguous whitespaces from whitespace tuple
+        contig_whitespace_length = whitespace[0]
+        whitespace_type = whitespace[1]
+
+        # At start of new line, add previous line's average contiguous space length to output
+        # and reset counter
+        if whitespace_type == '\n':
+
+            # Prevent 0 division error in average calculation for lines with no contiguous spaces
+            if current_total_spaces == 0:
+                current_num_contig_spaces = 1
+
+            contig_space_length_averages.append(current_total_spaces / current_num_contig_spaces)
+
+            # Reset counter for the next line
+            current_total_spaces = 0
+            current_num_contig_spaces = 0
+
+        # Continue counting number of spaces and number of contiguous space blocks for current line
+        else:
+            current_total_spaces += contig_whitespace_length
+            current_num_contig_spaces += 1
+
+    return contig_space_length_averages

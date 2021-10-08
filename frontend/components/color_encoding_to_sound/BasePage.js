4b47457a2963d401658d84b2fb18982d33e9284c
Felix Li <felixli5678@gmail.com>
2021-10-08 15:23:41 -0400

    Added react sketch color picker and added react-color as a dependency

--- frontend/components/color_encoding_to_sound/BasePage.js
+++ frontend/components/color_encoding_to_sound/BasePage.js
@@ -1,9 +1,24 @@
 import React from "react";
+import { SketchPicker } from 'react-color';
 
-const BasePage = () => {
-    return (<div>
-        <p> Hi </p>
-    </div>);
+class BasePage extends React.Component {
+    state = {
+        background: '#fff',
+    };
+
+    handleChangeComplete = (color) => {
+        this.setState({background: color.hex });
+    };
+
+    render() {
+        return (<div>
+            <p> Hi </p>
+            <SketchPicker
+            color={this.state.background}
+            onChangeComplete={this.handleChangeComplete}
+            />
+        </div>);
+    }
 };
 
 export default BasePage;

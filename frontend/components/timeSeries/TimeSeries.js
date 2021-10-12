5a737389535022754e260c5458dc8e4e0d86cc1a
Moises Trejo <43509746+mtrejo0@users.noreply.github.com>
2021-10-12 18:03:49 -0400

    added descriptor

--- frontend/components/timeSeries/TimeSeries.js
+++ frontend/components/timeSeries/TimeSeries.js
@@ -12,7 +12,14 @@ const TimeSeries = () => {
                 uploadSuccessfulCallback={setInstrumentSamples}
                 apiEndpoint={'api/generate_instrument_2d/'}
             />
-            <p>Upload a .csv file where each row represents the ratios for a single sound.</p>
+            <p>Upload a .csv file with <code>m</code> rows and <code>n</code> columns, and
+                define how each column <code>j</code> transforms the value in the column.<p>
+            </p>
+                We process these values by making <code>n</code> sounds.
+                <code> f(i,j)</code> represents a value at <code>(i,j)</code> in the csv.
+                We transform the value with <code>F(i,j) = (f(i,j) + offset_j) * multiplier_j + base_frequency_j</code>.
+                Each <code>F(i,j)</code> is made with the envelope defined for column <code>j</code>.
+            </p>
             {
                 instrumentSamples ? <>
                     <p><b>Sound:</b></p>

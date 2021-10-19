dca792b4b42ac509f3ec3be1cfe046d6052af485
vedadehhc <51378145+vedadehhc@users.noreply.github.com>
2021-10-19 18:50:33 -0400

    Added dropdown option to CustomizableInput and enabled flag

--- frontend/components/inputs/CustomizableInput.js
+++ frontend/components/inputs/CustomizableInput.js
@@ -6,18 +6,35 @@ import STYLES from "./CustomizableInput.module.scss";
  * A customizable input component. Consists of a wrapper div, a display description, and an
  * input component.
  */
-const CustomizableInput = ({type, name, display, getValue, setValue, ...rest}) => {
+const CustomizableInput = (
+    {type, name, display, getValue, setValue, enabled=true, options=[], ...rest}
+) => {
     return (
-        <div key={name} className={STYLES.customizableInputDiv}>
+        <div key={name} className={STYLES.customizableInputDiv} hidden={!enabled}>
             {display}
-            <input
-                type={type}
-                name={name}
-                className={STYLES.customizableInput}
-                onChange={(e) => setValue(e.target.value)}
-                value={getValue()}
-                {...rest}
-            />
+            {type === "dropdown"
+                ? <select
+                    name={name}
+                    className={STYLES.customizableInput}
+                    onChange={(e) => setValue(e.target.value)}
+                    value={getValue()}
+                    {...rest}
+                >
+                    {options.map((option) =>
+                        <option value={option.name} key={option.name}>
+                            {option.display}
+                        </option>
+                    )}
+                </select>
+                : <input
+                    type={type}
+                    name={name}
+                    className={STYLES.customizableInput}
+                    onChange={(e) => setValue(e.target.value)}
+                    value={getValue()}
+                    {...rest}
+                />
+            }
         </div>
     );
 };
@@ -28,6 +45,8 @@ CustomizableInput.propTypes = {
     display: PropTypes.string,
     getValue: PropTypes.func,
     setValue: PropTypes.func,
+    enabled: PropTypes.bool,
+    options: PropTypes.array,
 };
 
 export default CustomizableInput;

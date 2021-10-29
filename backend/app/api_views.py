77d56c85bb80ef96eb827f86215cf81f274372cd
Binh314 <binhp@mit.edu>
2021-10-29 15:23:00 -0400

    modified formatting

--- backend/app/api_views.py
+++ backend/app/api_views.py
@@ -143,8 +143,8 @@ def get_shape_analysis(request):
     if request.query_params.get('higherSecondFreq') == 'true':
         higher_second_freq = True
 
-    audio_data = text_shape_to_sound(text, secs_per_line, base_freq,
-                                                         max_beat_freq, higher_second_freq)
+    audio_data = text_shape_to_sound(text, secs_per_line, base_freq, max_beat_freq,
+                                     higher_second_freq)
 
     res = {
         'sound': audio_samples_to_wav_base64(audio_data)

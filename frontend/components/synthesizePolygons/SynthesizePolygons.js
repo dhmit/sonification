3af13e22a18357a36c2f55ab2868a975f3505138
vedadehhc <51378145+vedadehhc@users.noreply.github.com>
2021-10-31 17:37:19 -0400

    added adjustable panes

--- frontend/components/synthesizePolygons/SynthesizePolygons.js
+++ frontend/components/synthesizePolygons/SynthesizePolygons.js
@@ -23,6 +23,92 @@ const SynthesizePolygons = () => {
     const [ceilFrequency, setCeilFrequency] = useState(HIGH_FREQ);
     const audioRef = useRef(null);
 
+    // used for controlling overall pane
+    const [leftPaneWidth, setLeftPaneWidth] = useState(null);
+    const splitPaneVerticalRef = useRef(null);
+    const leftPaneRef = useRef(null);
+    const verticalSeparatorXPos = useRef(null);
+
+    // Used for controlling right pane
+    const [settingsPaneHeight, setSettingsPaneHeight] = useState(null);
+    const settingsRef = useRef(null);
+    const rightPaneRef = useRef(null);
+    const horizontalSeparatorYPos = useRef(null);
+
+    useEffect(() => {
+        if (!leftPaneWidth) {
+            setLeftPaneWidth(leftPaneRef.current.clientWidth);
+            leftPaneRef.current.style.flex = "none";
+        }
+
+        leftPaneRef.current.style.width = `${leftPaneWidth}px`;
+    }, [leftPaneWidth]);
+
+    useEffect(() => {
+        if (!settingsPaneHeight) {
+            setSettingsPaneHeight(settingsRef.current.clientHeight);
+            settingsRef.current.style.flex = "none";
+        }
+
+        settingsRef.current.style.height = `${settingsPaneHeight}px`;
+    }, [settingsPaneHeight]);
+
+    const onMouseDownVerticalSeparator = (e) => {
+        verticalSeparatorXPos.current = e.clientX;
+    };
+
+    const onMouseDownHorizontalSeparator = (e) => {
+        horizontalSeparatorYPos.current = e.clientY;
+    };
+
+    const onMouseMove = (e) => {
+        if (verticalSeparatorXPos.current) {
+            const newLeftWidth = leftPaneWidth + e.clientX - verticalSeparatorXPos.current;
+            verticalSeparatorXPos.current = e.clientX;
+
+            if (newLeftWidth <= 0) {
+                return leftPaneWidth !== 0 && setLeftPaneWidth(0);
+            }
+
+            const totalWidth = splitPaneVerticalRef.current.clientWidth;
+            if (newLeftWidth >= totalWidth) {
+                return leftPaneWidth !== 0 && setLeftPaneWidth(totalWidth);
+            }
+
+            setLeftPaneWidth(newLeftWidth);
+        } else if (horizontalSeparatorYPos.current) {
+            const newTopHeight = settingsPaneHeight + e.clientY- horizontalSeparatorYPos.current;
+            console.log(newTopHeight);
+            horizontalSeparatorYPos.current = e.clientY;
+
+            if (newTopHeight <= 0) {
+                return settingsPaneHeight !== 0 && setSettingsPaneHeight(0);
+            }
+
+            const totalHeight = rightPaneRef.current.clientHeight;
+            if (newTopHeight >= totalHeight) {
+                return settingsPaneHeight !== 0 && setSettingsPaneHeight(totalHeight);
+            }
+
+            setSettingsPaneHeight(newTopHeight);
+        }
+    };
+
+    const onMouseUp = () => {
+        verticalSeparatorXPos.current = null;
+        horizontalSeparatorYPos.current = null;
+    };
+
+    React.useEffect(() => {
+        document.addEventListener("mousemove", onMouseMove);
+        document.addEventListener("mouseup", onMouseUp);
+
+        return () => {
+            document.removeEventListener("mousemove", onMouseMove);
+            document.removeEventListener("mouseup", onMouseUp);
+        };
+    });
+
     useEffect(() => {
         if (data) {
             if (data["sound"]) {
@@ -157,12 +243,14 @@ const SynthesizePolygons = () => {
     return (
         <div>
             <h1>Synthesize Polygons</h1>
-            <div className={STYLES.pageGrid}>
-                <div className={STYLES.polygonEditor}>
-                    <h2>Input Polygon</h2>
+            <div className={STYLES.splitPane} ref={splitPaneVerticalRef}>
+                <div className={STYLES.leftPane} ref={leftPaneRef}>
+                    <h2>Polygon Editor</h2>
                     <p>
-                        Files should be CSVs that have two columns that include x and y coordinates of the
-                        points of the polygon with x-coordinates in the first column and the corresponding
+                        Files should be CSVs that have two columns that include x and y coordinates
+                        of the
+                        points of the polygon with x-coordinates in the first column and the
+                        corresponding
                         y-coordinates in the second column.
                     </p>
                     <PolygonEditor
@@ -173,36 +261,37 @@ const SynthesizePolygons = () => {
                     />
                     {data
                         ? <>
-                            <PolygonViewer width={300} height={300} rawPoints={data["points"]} currentTime={curAudioTime} timestamps={timestamps}/>
+                            <PolygonViewer width={300} height={300} rawPoints={data["points"]}
+                                           currentTime={curAudioTime} timestamps={timestamps}/>
                         </>
                         : <p>Upload a CSV or draw a polygon above to get results! </p>
                     }
                 </div>
-
-                <div className={STYLES.editorSettings}>
-                    <h2>Settings</h2>
-                    {userOptions.map((option) => <CustomizableInput key={option.name} {...option}/>)}
+                <div className={STYLES.paneSeparatorVertical} onMouseDown={onMouseDownVerticalSeparator}/>
+                <div className={STYLES.rightPane} ref={rightPaneRef}>
+                    <div className={STYLES.editorSettings} ref={settingsRef}>
+                        <h2>Settings</h2>
+                        {userOptions.map((option) => <CustomizableInput
+                            key={option.name} {...option}/>)}
+                    </div>
+                    <div className={STYLES.paneSeparatorHorizontal} onMouseDown={onMouseDownHorizontalSeparator}/>
+                    <div className={STYLES.playback}>
+                        <h2>Results</h2>
+                        {data
+                            ? <audio
+                                controls
+                                controlsList={"nodownload"}
+                                ref={audioRef}
+                                onTimeUpdate={() => {
+                                    setCurAudioTime(audioRef.current.currentTime);
+                                }}
+                            >
+                                <source src={`data:audio/wav;base64,${sound}`} type={"audio/wav"}/>
+                            </audio>
+                            : <p>Upload a CSV or draw a polygon in the editor to get results! </p>
+                        }
+                    </div>
                 </div>
-
-                <div className={STYLES.playback}>
-                    <h2>Results</h2>
-                    {data
-                        ? <>
-                            <audio
-                        controls
-                        controlsList={"nodownload"}
-                        ref={audioRef}
-                        onTimeUpdate={() => {
-                            setCurAudioTime(audioRef.current.currentTime);
-                        }}
-                    >
-                        <source src={`data:audio/wav;base64,${sound}`} type={"audio/wav"}/>
-                    </audio>
-                        </>
-                        : <p>Upload a CSV or draw a polygon in the editor to get results! </p>
-                    }
-                </div>
-
             </div>
         </div>
     );

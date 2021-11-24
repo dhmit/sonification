a0fcfd3a101948eed1a7fabde7996ab587c7005a
Peihua Huang <nemesis2126@gmail.com>
2021-11-24 12:11:35 -0500

    Revert "Created palette change alert"
    This reverts commit 15881d46d9ddb1168ac0716c9895d1c1ef969e77.

--- frontend/components/color/ColorSonifier.js
+++ frontend/components/color/ColorSonifier.js
@@ -24,19 +24,17 @@ class ColorSonifier extends React.Component {
             instrumentGenerated: false,
             colorPickerColor: {r: 51, g: 51, b: 51},
             listOfColors: initialColors,
-            listOfColorsChanged: false,
         };
     }
 
     handlePaletteClick = (e) => {
-        this.setState({selected: Number(e.target.id),});
+        this.setState({selected: Number(e.target.id)});
     }
 
     handleChangeComplete = (color) => {
         const currentColorState = [...this.state.listOfColors];
         currentColorState[this.state.selected] = color.rgb;
-        this.setState({listOfColors: [...currentColorState], colorPickerColor: color.rgb,
-            listOfColorsChanged: true,});
+        this.setState({listOfColors: [...currentColorState], colorPickerColor: color.rgb});
     };
 
     handleSubmit = () => {
@@ -45,7 +43,6 @@ class ColorSonifier extends React.Component {
             this.setState({
                 result: responseDict,
                 instrumentGenerated: true,
-                listOfColorsChanged: false,
             });
             alert("Instrument has been updated!");
         };
@@ -61,12 +58,6 @@ class ColorSonifier extends React.Component {
                 handlePaletteClick={this.handlePaletteClick}
             />
         );
-        var updateAlert = "";
-        const listOfColorsChanged = this.state.listOfColorsChanged;
-        if (listOfColorsChanged) {
-            updateAlert = "The palette has changed, please update the instrument.";
-        }
-
         return (<div>
             <h1>Colors</h1>
             <p> Use the color picker below to choose a color, and hit the <b>submit</b> button
@@ -87,9 +78,6 @@ class ColorSonifier extends React.Component {
                             ? "Update Instrument"
                             : "Generate Instrument"}
                     </button>
-                    <div>
-                        {updateAlert}
-                    </div>
                 </div>
 
                 <div className="col-sm">

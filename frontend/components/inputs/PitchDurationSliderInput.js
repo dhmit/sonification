db93c5de8421a05b8db55e0d576cfa6fca99b857
Peihua Huang <nemesis2126@gmail.com>
2021-12-03 15:53:28 -0500

    Updating styling of double sliders

--- frontend/components/inputs/PitchDurationSliderInput.js
+++ frontend/components/inputs/PitchDurationSliderInput.js
@@ -2,7 +2,9 @@ import React, {useState} from "react";
 import PropTypes from "prop-types";
 import Slider from '@mui/material/Slider';
 
-const PitchDurationSliderInput = ({minPitch, maxPitch, minDuration, maxDuration}) => {
+// updateValues is a function that takes in the value and the slider to be updated
+const PitchDurationSliderInput = (
+    {minPitch, maxPitch, minDuration, maxDuration, updateValues}) => {
     const [durationValues, setDurationValues] = useState([minDuration, maxDuration]);
     const [pitchValues, setPitchValues] = useState([minPitch, maxPitch]);
 
@@ -10,17 +12,23 @@ const PitchDurationSliderInput = ({minPitch, maxPitch, minDuration, maxDuration}
     const updateDurationSlider = (event, newValues) => {
         event.preventDefault();
         setDurationValues(newValues);
+        updateValues(newValues, "duration");
     };
 
     const updatePitchSlider= (event, newValues) => {
         event.preventDefault();
         setPitchValues(newValues);
+        updateValues(newValues, "pitch");
     };
 
     return(
         <>
-            <div>
-                <Slider
+            <b>Duration</b>
+            <div className="row">
+                <div className="col-2">
+                    {durationValues[0]} secs
+                </div>
+                <Slider className="col"
                     getAriaLabel={() => 'Duration'}
                     value={durationValues}
                     onChange={updateDurationSlider}
@@ -29,16 +37,27 @@ const PitchDurationSliderInput = ({minPitch, maxPitch, minDuration, maxDuration}
                     max={maxDuration}
                     step={0.01}
                 />
-             </div>
-            <div>
-                <Slider
+                <div className="col-2">
+                    {durationValues[1]} secs
+                </div>
+            </div>
+            <b>Pitch</b>
+            <div className="row">
+                <div className="col-2">
+                    {pitchValues[0]} Hz
+                </div>
+                <Slider className="col"
                     getAriaLabel={() => 'Duration'}
                     value={pitchValues}
                     onChange={updatePitchSlider}
                     valueLabelDisplay="auto"
                     min={minPitch}
                     max={maxPitch}
+
                 />
+                <div className="col-2">
+                    {pitchValues[1]} Hz
+                </div>
             </div>
         </>);
 };
@@ -48,6 +67,7 @@ PitchDurationSliderInput.propTypes = {
     maxPitch: PropTypes.number,
     minDuration: PropTypes.number,
     maxDuration: PropTypes.number,
+    updateValues: PropTypes.func,
 };
 
 export default PitchDurationSliderInput;

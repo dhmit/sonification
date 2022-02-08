7f9979cf54efa6d71a34a24ee421fc69755c695e
Ryaan Ahmed <mail@ryaanahmed.com>
2022-02-07 22:30:22 -0500

    lint

--- frontend/components/inputs/UploadTimeSeriesFileInput.js
+++ frontend/components/inputs/UploadTimeSeriesFileInput.js
@@ -190,7 +190,7 @@ const UploadTimeSeriesFileInput = ({
                         </div>
                     </div>
                 </form>
-            <hr />
+                <hr />
             </>}
         </>
     );

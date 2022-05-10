c4cecfd00080d9a44e4f2d74eed087d0d144d2f8
Ryaan Ahmed <rahmed@mit.edu>
2022-05-10 12:00:51 -0400

    add audio, photos, student quotes

---
+++ frontend/studentQuotes.js
@@ -0,0 +1,44 @@
+// COLORS
+export const EMEKA_QUOTE = {
+    quote: "My favorite part of working on the colors feature was collaborating to figure out" +
+        " what the final representation of a color would be.",
+    speaker: "Emeka E.",
+};
+
+// Gestures:
+export const GRACE_QUOTE = {
+    quote: "It was really cool to see the intersections between math and programming and music, and how all of that could come together to form a final project. [...] It really helped me understand how the different pieces fit together to form a product.",
+    speaker: "Grace J.",
+};
+
+export const PEIHUA_QUOTE = {
+    quote: "My favorite part of the project was learning more about how pitches and notes work and seeing our final feature at the end. I personally don’t have any musical background, so a lot of this was new to me. [...] Normally when you look at something visual, you don’t think ‘Oh, this is what it would sound like.’",
+    speaker: "Peihua H.",
+};
+
+// Polygons
+export const ANGELINA_QUOTE = {
+    quote: "This project is all about making tools to get people to play with sound in hopefully new ways, so of course we had to ourselves stretch what we knew to different shapes. That creative process was really fun.",
+    speaker: "Angelina W.",
+};
+
+export const QUINCY_QUOTE = {
+    quote: "I really enjoyed being able to combine my computer science and musical background to create something unique and memorable.",
+    speaker: "Quincy J",
+};
+
+// Time Series
+export const EESHA_QUOTE = {
+    quote: "I really enjoyed was the imagination that had to go into it in order to find connections between two different types of data.",
+    speaker: "Eesha B.",
+};
+
+export const MOISES_QUOTE = {
+    quote: "I really enjoyed being able to use computer science to make something out of nothing. It felt like I could make music out of thin air, out of sunset data, out of CO2 emission data, and use it to make something that could move people.",
+    speaker: "Moises T.",
+};
+
+export const StudentQuote = (quoteData) => {
+    // TODO(ra): Implement me!
+    return null;
+};
